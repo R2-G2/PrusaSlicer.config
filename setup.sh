3ee0f9473df8df8614abf7ddfed23e3ebd626311
Ralf Grawunder <r.grawunder@googlemail.com>
2021-12-22 02:02:19 +0100

    [PS.c] Setup.
    Updated setup script and ReadMe.
    
    Signed-off-by: Ralf Grawunder <r.grawunder@googlemail.com>

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ dir="${HOME}/.config/PrusaSlicer";
 cd "$(dirname "$(readlink -f "${0}";)";)/";
 
 for config in $(ls -d */ | cut -d"/" -f1;); do
-    cp -vr "${dir}/${config}" .;
-    rm -vrf "${dir}/${config}";
-    ln -vs "${PWD}/${config}" "${dir}/${config}";
+    cp -vr "${dir}/${config}" . && rm -vrf "${dir}/${config}";
+    ln -vs "${PWD}/${config}" "${dir}";
 done;
